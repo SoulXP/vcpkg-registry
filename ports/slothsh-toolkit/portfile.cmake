b32bcaf1d5912c06e1456f6c31dae31da3df7140
Stefan Olivier <s.olivier1194@gmail.com>
2023-01-08 16:22:12 +0200

    added SHA512 for slothsh-toolkit 0.0.1

--- ports/slothsh-toolkit/portfile.cmake
+++ ports/slothsh-toolkit/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO SoulXP/slothsh-toolkit
     REF 5ab4266f0038eba56856ff42fe6277646b9a9a89
-    SHA512 0
+    SHA512 0c7a5072ecc63fda7d444912d03c4eba5c5ea4a4e22114ba778cb80ebec98aa9a3f505108b6e6a64169e532e0f65f49544a505a16257a26934a76ce0e3d73506
     HEAD_REF main
     )
 
