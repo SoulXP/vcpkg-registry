ec1c498f4dd240f6c0075bea391d2974c3cc5f3d
Stefan Olivier <s.olivier1194@gmail.com>
2023-01-09 15:06:53 +0200

    modified ports

--- ports/slothsh-toolkit/portfile.cmake
+++ ports/slothsh-toolkit/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO SoulXP/slothsh-toolkit
-    REF 0ede2b9a39ac048b83c9bc8c581ef57eb9ba99e2
-    SHA512 ad0f044eb65acd2c5abd5eeca41407afbc429b30b999ce4444b9047493fd0fb79fe4a5faa7f97f72e337fe1f3881cd31940afae6daae994df564a9ba472d8e49
+    REF a8d542bf1800e8e1ab526fe0753c0af28540afe1
+    SHA512 0
     HEAD_REF main
     )
 
@@ -18,7 +18,7 @@ vcpkg_configure_cmake(
 vcpkg_install_cmake()
 vcpkg_cmake_config_fixup(PACKAGE_NAME slothshtoolkit)
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
 file(INSTALL "${SOURCE_PATH}/LICENSE"
      DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
