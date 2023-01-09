e3db2590ab1fca9b946a4034ffc690b2ea1abf42
Stefan Olivier <s.olivier1194@gmail.com>
2023-01-09 03:24:33 +0200

    updated latest slothsh-toolkit SHA512 hash

--- ports/slothsh-toolkit/portfile.cmake
+++ ports/slothsh-toolkit/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO SoulXP/slothsh-toolkit
     REF ce9864a9711400ba2e7a7b4148a521ed0aa8a47b
-    SHA512 0
+    SHA512 c40008ea5de1dc2dfb2cc1463fdac0a02c2036ef4afdf6ca733f53b4b117a84fde8cd5e728cb87da356ff694ad0914b7e4ae96d573e2ab4ac6aeb2e107998aae
     HEAD_REF main
     )
 
