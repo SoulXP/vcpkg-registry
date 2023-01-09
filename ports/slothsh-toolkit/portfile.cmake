59ad5306a76266c7b734d12971c323b77c4a6da5
Stefan Olivier <s.olivier1194@gmail.com>
2023-01-09 05:12:48 +0200

    updated SHA512 sum

--- ports/slothsh-toolkit/portfile.cmake
+++ ports/slothsh-toolkit/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO SoulXP/slothsh-toolkit
     REF cb8aa1dfd48525cf2a34495754ae6cb0d9a72317
-    SHA512 0
+    SHA512 53a263e7edeeac7718b9109c15e4f4531bd8b2e6fe636e204bea25d8c8e5c9fc3b3907a3bd92c8a833a762be7cb9c638db4655a7d1a29ab855f526b3653e2d6b
     HEAD_REF main
     )
 
