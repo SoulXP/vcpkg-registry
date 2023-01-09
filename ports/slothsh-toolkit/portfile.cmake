643f846c6a7a8f94fb8f413d28e718fc45c50a89
Stefan Olivier <s.olivier1194@gmail.com>
2023-01-09 03:22:25 +0200

    new slothsh-toolkit upstream commit

--- ports/slothsh-toolkit/portfile.cmake
+++ ports/slothsh-toolkit/portfile.cmake
@@ -1,8 +1,8 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO SoulXP/slothsh-toolkit
-    REF acd58e533c84df0ab22640439fde4237dc3b63fe
-    SHA512 1e46882e418ebe9cdfaf2a26a5137b136b92c741606c8b8636af3b9fee41f2ae78bf901bf82025338540589e2822d2ce42cd550e2c9dffc9a34b6535a4b5f00b
+    REF ce9864a9711400ba2e7a7b4148a521ed0aa8a47b
+    SHA512 0
     HEAD_REF main
     )
 
